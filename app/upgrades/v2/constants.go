d2677e9f4a2d534b61d5e613c14d426c794f0da0
Ran Mishael <ran@lavanet.xyz>
2022-08-11 11:53:28 +0200

    removing comment

--- app/upgrades/v2/constants.go
+++ app/upgrades/v2/constants.go
@@ -5,7 +5,6 @@ import (
 	"github.com/lavanet/lava/app/upgrades"
 )
 
-// UpgradeName defines the on-chain upgrade name for the Osmosis v11 upgrade.
 const UpgradeName = "v2"
 
 var Upgrade = upgrades.Upgrade{
